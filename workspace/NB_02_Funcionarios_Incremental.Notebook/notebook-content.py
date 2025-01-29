9d7d3daecbfea098542caf3b3b9a381cd866da61
Alison Pezzott <58135934+alisonpezzott@users.noreply.github.com>
2025-01-29 19:08:25 -0300

    Committing 1 item from workspace cb84b88f-2b8c-480e-9c0b-2e37850e3d4b

--- workspace/NB_02_Funcionarios_Incremental.Notebook/notebook-content.py
+++ workspace/NB_02_Funcionarios_Incremental.Notebook/notebook-content.py
@@ -9,7 +9,7 @@
 # META   "dependencies": {
 # META     "lakehouse": {
 # META       "default_lakehouse": "11540624-ed9f-41e1-9b0e-6bd60e915153",
-# META       "default_lakehouse_name": "LK01",
+# META       "default_lakehouse_name": "LK_01",
 # META       "default_lakehouse_workspace_id": "cb84b88f-2b8c-480e-9c0b-2e37850e3d4b",
 # META       "known_lakehouses": [
 # META         {
